2b302471d44602a5013294887ee2bf049c4d6211
LuuNguyenHanhDuyen <luuduyen1206@gmail.com>
2025-03-19 16:40:52 +0700

    feat(product.controller.js): Add product OSP-44

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,30 +1,69 @@
-import Product from "../models/product.model.js"
+import Product from "../models/product.model.js";
 
 export const getProducts = async (req, res, next) => {
   try {
-    const products = Product.find()
+    const products = await Product.find();
 
     res.status(200).json({
       success: true,
       message: "Get products successfully",
-      data: { products }
-    })
+      data: { products },
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const getProduct = async (req, res, next) => {
   try {
-    const { productId } = req.body
-    const product = Product.findOne({ _id: productId })
+    const { productId } = req.params;
+    const product = await Product.findOne({ _id: productId });
 
     res.status(200).json({
       success: true,
       message: "Get product successfully",
-      data: { product }
-    })
+      data: { product },
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+export const addProduct = async (req, res, next) => {
+  try {
+    const { name, description, price, stock, category } = req.body;
+
+    if (!name || !price || stock === undefined) {
+      return res.status(400).json({
+        success: false,
+        message: "Name, price, and stock are required",
+      });
+    }
+
+    const imageUrl = req.files.imageUrl
+      ? req.files.imageUrl[0].path // Cloudinary trả về `path` là URL ảnh
+      : null;
+
+    const detailImages = req.files.detailImages
+      ? req.files.detailImages.map((file) => file.path)
+      : [];
+
+    const newProduct = await Product.create({
+      name,
+      description,
+      price,
+      stock,
+      category,
+      imageUrl,
+      detailImages,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      data: newProduct,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,7 +1,20 @@
-import { Router } from "express"
-import { getProduct, getProducts } from "../controllers/product.controller.js"
+import { Router } from "express";
+import {
+  addProduct,
+  getProduct,
+  getProducts,
+} from "../controllers/product.controller.js";
+import upload from "../multer/handle-upload.js";
 
-const productRouter = Router()
-productRouter.get("/", getProducts)
-productRouter.get("/:productId", getProduct)
-export default productRouter
+const productRouter = Router();
+productRouter.get("/", getProducts);
+productRouter.get("/:productId", getProduct);
+productRouter.post(
+  "/add",
+  upload.fields([
+    { name: "imageUrl", maxCount: 1 },
+    { name: "detailImages", maxCount: 10 },
+  ]),
+  addProduct
+);
+export default productRouter;
