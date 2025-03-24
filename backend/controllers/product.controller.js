4f9ce8e8ead4fe826a9b84e5315ca9589ce8b034
LuuNguyenHanhDuyen <luuduyen1206@gmail.com>
2025-03-24 14:03:27 +0700

    feature(product.controller.js): Add pagination

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -5,16 +5,40 @@ import handleError from "../config/error-handler.js";
 
 export const getProducts = async (req, res, next) => {
   try {
-    const products = await Product.find();
+    let { page = 1, limit = 2 } = req.query;
+    page = parseInt(page);
+    limit = parseInt(limit);
+
+    if (page < 1 || limit < 1) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        success: false,
+        message: "Page and limit must be positive numbers",
+      });
+    }
+
+    const totalProducts = await Product.countDocuments();
+    const products = await Product.find()
+      .limit(limit)
+      .skip((page - 1) * limit);
+
     const returnData = new ReturnData();
     returnData.success = true;
     returnData.message = "Get products successfully";
-    returnData.data = products;
+    returnData.data = {
+      products,
+      pagination: {
+        currentPage: page,
+        totalPages: Math.ceil(totalProducts / limit),
+        totalProducts,
+      },
+    };
+
     res.status(HTTP_STATUS.OK).json(returnData.toObject());
   } catch (error) {
     handleError(res, error);
   }
 };
+
 
 export const getProduct = async (req, res, next) => {
   try {
@@ -127,22 +151,28 @@ export const deleteProduct = async (req, res, next) => {
 
 export const searchProduct = async (req, res, next) => {
   try {
-    const { query } = req.query;
+    let { query, page = 1, limit = 2 } = req.query;
     if (!query) {
-      return handleError(
-        res,
-        "Search query is required",
-        HTTP_STATUS.BAD_REQUEST
-      );
+      getProducts(req,res,next);
     }
-
-    const products = await Product.find({
+    page = +page;
+    limit = +limit;
+    if (page < 1 || limit < 1) {
+      return handleError(res, "Page and limit must be positive numbers", HTTP_STATUS.BAD_REQUEST);
+    }
+    const searchCondition = {
       $or: [
         { name: { $regex: query, $options: "i" } },
         { category: { $regex: query, $options: "i" } },
         { description: { $regex: query, $options: "i" } },
       ],
-    });
+    };
+    const totalProducts = await Product.countDocuments(searchCondition);
+
+    // Truy vấn với limit và skip
+    const products = await Product.find(searchCondition)
+      .limit(limit)
+      .skip((page - 1) * limit);
 
     if (products.length === 0) {
       return handleError(res, "No products found", HTTP_STATUS.NOT_FOUND);
@@ -151,7 +181,15 @@ export const searchProduct = async (req, res, next) => {
     const returnData = new ReturnData();
     returnData.success = true;
     returnData.message = "Products found";
-    returnData.data = products;
+    returnData.data = {
+      products,
+      pagination: {
+        currentPage: page,
+        totalPages: Math.ceil(totalProducts / limit),
+        totalProducts,
+      },
+    };
+
     res.status(HTTP_STATUS.OK).json(returnData.toObject());
   } catch (error) {
     handleError(res, error);
