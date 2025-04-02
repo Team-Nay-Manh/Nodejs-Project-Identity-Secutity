ea4c983e807274d9211be89074551cb470a4227e
LuuNguyenHanhDuyen <luuduyen1206@gmail.com>
2025-04-02 17:09:38 +0700

    refactor(product): Remove unnecessary comments

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -3,7 +3,7 @@ import apiRequest from "../config/axios"
 export const fetchProducts = async () => {
   try {
     const response = await apiRequest.get("/api/v1/products/")
-    return response.data.data.products // Assuming the products are in `data.data`
+    return response.data.data.products
   } catch (error) {
     console.error("Error fetching products:", error)
     throw error
@@ -13,8 +13,8 @@ export const fetchProducts = async () => {
 export const fetchCategories = async () => {
   try {
     const response = await apiRequest.get("/api/v1/categories/")
-    console.log("Fetched categories:", response.data.data) // Debugging log
-    return response.data.data // Assuming categories are in `data`
+    console.log("Fetched categories:", response.data.data)
+    return response.data.data
   } catch (error) {
     console.error("Error fetching categories:", error)
     throw error
