171ab72768b7e15423e4faa4c0837495f4fea122
BuiChiBao <buichibao1601@gmail.com>
2025-04-04 23:25:08 +0700

    bugfix(route): Change method

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,18 +1,21 @@
 import { Router } from "express"
-import { getUser, getUsers } from "../controllers/user.controller.js"
+import {
+  getUser,
+  getUsers,
+  toggleUserRole
+} from "../controllers/user.controller.js"
 import { authorize } from "../middlewares/auth.middlewares.js"
 
 const userRouter = Router()
 
-userRouter.get("/", getUsers)
+userRouter.get("/", authorize, getUsers)
 userRouter.get("/:id", authorize, getUser)
-
 userRouter.put("/:id", (req, res) => {
   return res.send({ title: "Update user" })
 })
-
 userRouter.delete("/:id", (req, res) => {
   return res.send({ title: "Delete user" })
 })
+userRouter.put("/:id/role", authorize, toggleUserRole)
 
 export default userRouter
