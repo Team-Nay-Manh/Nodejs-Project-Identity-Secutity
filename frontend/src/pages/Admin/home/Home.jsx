96e2f84cafc62c0e023dc751df24dc749a70e9c5
Bui Hoang Viet <bhviet.910lhphong.cr@gmail.com>
2025-04-07 17:50:02 +0700

    chore: Update deps

--- frontend/src/pages/Admin/home/Home.jsx
+++ frontend/src/pages/Admin/home/Home.jsx
@@ -1,7 +1,7 @@
 import Header from "../../../components/Admin/header/Header"
 import Insights from "../../../components/Admin/insights/Insights"
-import RecentOrders from "../../../components/Admin/orders/RecentOrders"
 import Reminders from "../../../components/Admin/reminders/Reminders"
+import Orders from "../../../components/Admin/orders/Orders"
 import "./home.css"
 
 function Home() {
